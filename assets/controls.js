27cda25bbe8cb0550267250b6f34a7f4d752f319
Jens Meinecke <1739138+meinjens@users.noreply.github.com>
2021-11-12 13:43:37 +0100

    feat: connection handling to ws endpoints

--- assets/controls.js
+++ assets/controls.js
@@ -1,7 +1,36 @@
 import nipplejs from 'nipplejs';
 
+let steering_engine_left = null;
+let steering_engine_right = null;
+
 jquery(document).ready(function() {
     jquery(".gas").knob();
+
+    steering_engine_left = new WebSocket('ws://localhost:5000/ws/steering/left');
+    steering_engine_right = new WebSocket('ws://localhost:5000/ws/steering/right');
+
+    steering_engine_left.onopen = function() {
+        console.log('[left] connection to engine');
+    }
+    steering_engine_left.onclose = function(event) {
+        if (event.wasClean) {
+            console.log(`[left] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+            console.log(event);
+        } else {
+            console.log('[left] Connection died');
+        }
+    }
+    steering_engine_right.onopen = function() {
+        console.log('[right] connection to engine');
+    }
+    steering_engine_right.onclose = function(event) {
+        if (event.wasClean) {
+            console.log(`[right] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+            console.log(event);
+        } else {
+            console.log('[right] Connection died');
+        }
+    }
 
     const steering_left = nipplejs.create({
         zone: document.getElementById('engine_left'),
@@ -20,11 +49,20 @@ jquery(document).ready(function() {
         .on('move', function(event, data){
             if (data.direction) {
                 console.log('move left: ' + data.direction.y + ' ' + data.distance);
+                const steering = {
+                    'dir': data.direction.y,
+                    'dist': data.distance
+                };
+                steering_engine_left.send(JSON.stringify(steering));
             }
             
         })
         .on('end', function(event, data) {
-            console.log('stop engine left');
+            const steering = {
+                'dir': 'center',
+                'dist': 0
+            };
+            steering_engine_left.send(JSON.stringify(steering));
         })
 
     const steering_right = nipplejs.create({
@@ -44,10 +82,20 @@ jquery(document).ready(function() {
         .on('move', function(event, data){
             if (data.direction) {
                 console.log('move left: ' + data.direction.y + ' ' + data.distance);
+                const steering = {
+                    'dir': data.direction.y,
+                    'dist': data.distance
+                };
+                steering_engine_right.send(JSON.stringify(steering));
             }
         })
         .on('end', function(event, data) {
             console.log('stop engine right');
+            const steering = {
+                'dir': 'center',
+                'dist': 0
+            };
+            steering_engine_right.send(JSON.stringify(steering));
         })
 
 });
